b29a5465c9082ed3f63fc79bad1057cb6f1aff91
dimin75 <33786536+dimin75@users.noreply.github.com>
2025-01-13 13:35:27 +1000

    Update main.py more commits

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("Hello, world. This is an example of CI/CD-automation for GitHub project. 'Test is succeeded, you see the message from my-host.hk'")
+print("More strings to test is coming...")
 print("Done!")
